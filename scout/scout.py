137f6a6424c909f9899b1e565222fd5593f30ced
Brandon Powers <brandon-powers@users.noreply.github.com>
2017-09-28 00:30:37 -0400

    Determine the max available block in a date range

--- scout/scout.py
+++ scout/scout.py
@@ -5,6 +5,8 @@ from oauth2client.file import Storage
 import httplib2
 import os
 import json
+import pytz
+from dateutil.parser import parse
 from datetime import datetime, timedelta
 
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
@@ -13,26 +15,54 @@ APPLICATION_NAME = 'scout'
 
 def get_most_available_block(service, calendar_id, start_time, end_time):
     # break up date range to array of start/end date ranges for each day
-    s = datetime.strptime(start_time, '%Y-%m-%d')
-    e = datetime.strptime(end_time, '%Y-%m-%d')
+    s = parse(start_time)
+    e = parse(end_time)
     ranges = []
     while s.day <= e.day:
         _s = datetime(s.year, s.month, s.day, 13, 0, 0)
-        start = _s.strftime('%Y-%m-%dT%H:%M:%SZ')
-        end = (_s + timedelta(hours=8)).strftime('%Y-%m-%dT%H:%M:%SZ')
+        start = pytz.utc.localize(_s).isoformat()
+        end = pytz.utc.localize(_s + timedelta(hours=8)).isoformat()
         ranges.append((start, end))
         s += timedelta(days=1)
 
-    # get events
+    real_max = timedelta()
+
+    # for each day
     for r in ranges:
+
+        # get events for that day
         eventsResult = service.events().list(
                 calendarId=calendar_id, timeMin=r[0], timeMax=r[1], 
                 singleEvents=True, orderBy='startTime').execute()
         events = eventsResult.get('items', [])
+
         print('************')
+
+        # get event_ranges for each event on given day
+        event_ranges = []
         for d in events:
-            print(json.dumps(d, indent=4))
+            start = d['start']
+            if not start.get('dateTime'):
+                continue
+            start = parse(start['dateTime'])
+            end = parse(d['end']['dateTime'])
+            event_ranges.append((start, end))
+
+        # use event_ranges to find max_range for that day
+        max_diff = timedelta()
+        for v in event_ranges:
+            print(v[0].isoformat() + ' --- ' + v[1].isoformat())
+            diff = v[1] - v[0]
+            if max_diff < diff:
+                max_diff = diff
+        print('MAX: ' + str(max_diff))
+
+        if real_max < max_diff:
+            real_max = max_diff
+
         print('************')
+
+    print('FOR ALL DAYS: ' + str(real_max))
 
     return events
 
@@ -57,7 +87,7 @@ def main():
     service = discovery.build('calendar', 'v3', http=http)
 
     calendar_id = 'primary'
-    start_time = '2017-09-20'
+    start_time = '2017-09-10'
     end_time = '2017-09-26'
 
     diff = get_most_available_block(service, calendar_id, start_time, end_time)
