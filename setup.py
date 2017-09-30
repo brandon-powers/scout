144d89241c1e7afcf373dc9d725f532e87c8ecfc
Brandon Powers <brandon-powers@users.noreply.github.com>
2017-09-30 02:15:07 -0400

    Add OAuth & API client dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
-setup(name='scout',
+setup(name='Scout',
       version='0.1.0',
-      description='LFM Google Calendar Discovery',
+      description='A Google Calendar discovery tool',
       url='https://github.com/brandon-powers/scout',
       author='Brandon Powers',
       author_email='brandon.powers@listenfirstmedia.com',
-      packages=['scout'])
+      packages=['scout'],
+      install_requires=[
+          'google-auth',
+          'google-api-python-client'
+      ])
